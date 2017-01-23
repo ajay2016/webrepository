fdcd758f98aa7f1d9f586aa9b1a4ae411afff63d
Ajay <ajay.skiva@gmail.com>
2017-01-23 16:04:56 +1100

    initial commit

---
+++ SumHundred.java
@@ -0,0 +1,21 @@
+package module1;
+
+
+
+public class SumHundred {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int  sum = 0;
+for (int i = 0; i<100 ; i ++)
+
+{
+
+sum = sum + i;
+ 
+}
+System.out.println("the sum is  " +sum);	
+	
+	}
+
+}
